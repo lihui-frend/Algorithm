3d7f831ada0c0c3fc9208bb2f620e7df7e319369
lihui-frend <lihui_frend@163.com>
2018-07-29 15:25:55 +0800

    personal

---
+++ src/com/algorithm/Reverse.java
@@ -0,0 +1,44 @@
+package com.algorithm;
+
+class Node{
+    private int data;
+    private Node next;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+public class Reverse {
+    //递归
+    public static Node reverse1(Node head){
+        if (head==null||head.getNext()==null){
+            return head;
+        }
+        Node result = reverse(head.getNext());
+        head.getNext().setNext(head);
+        head.setNext(null);
+        return result;
+    }
+
+    //遍历
+    public static Node reverse2(Node head){
+        if(head==null||head.getNext()==null){
+            return head;
+        }
+        Node pre = head;
+        Node cur = head.getNext();
+        Node tep;
+        while (cur!=null){
+            tep = cur.getNext();
+            cur.setNext(pre);
+            pre = cur;
+            cur = tep;
+        }
+        head.setNext(null);
+        return pre;
+    }
+}
