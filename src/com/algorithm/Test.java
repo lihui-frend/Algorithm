1dcc4fc5e33bdead72ea1046daef0961a6a8f718
lihui-frend <lihui_frend@163.com>
2018-08-04 20:52:51 +0800

    personal

--- src/com/algorithm/Test.java
+++ src/com/algorithm/Test.java
@@ -1,5 +1,7 @@
 package com.algorithm;
 
+import java.util.Arrays;
+
 public class Test {
     public static String Compression(String string){
 
@@ -16,8 +18,24 @@ public class Test {
         }
         return stringBuilder.toString();
     }
+
+    public static int[] target(int arr[],int target){
+        int length = arr.length;
+        int [] result = new int[2];
+        for (int i=0;i<length-1;i++){
+            for (int j=i+1;j<length;j++){
+                if (arr[i]+arr[j]==target){
+                    result[0] = i+1;
+                    result[1] = j+1;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
     public static void main(String [] args){
         String str = "aaaabbcccddeeeeefghij";
-        System.out.print(Compression(str));
+        int[] arr = {33,1,11,29,1,1};
+        System.out.print(Arrays.toString(target(arr,2)));
     }
 }
